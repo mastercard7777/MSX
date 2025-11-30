d48b2fff9de78e905cb4ba1bbaebe55c7226d956
NS <sles3281@gmail.com>
2025-11-30 15:35:48 +0900

    MSL manifest

--- MSL/main.js
+++ MSL/main.js
@@ -1,1 +1,30 @@
-ejd
+{
+  "format_version": 2,
+  "header": {
+    "description": "MSL V2.1",
+    "name": "MSL V2.1",
+    "uuid": "9eaf8748-1ce3-4f58-8a7c-e20aedd6f775",
+    "version": [
+      0,
+      2,
+      1
+    ],
+    "min_engine_version": [
+      1,
+      21,
+      90
+    ]
+  },
+  "modules": [
+    {
+      "description": "Data module",
+      "type": "data",
+      "uuid": "0fea32c3-755e-4fd4-beac-97b1053cd394",
+      "version": [
+        0,
+        2,
+        1
+      ]
+    }
+  ]
+}
